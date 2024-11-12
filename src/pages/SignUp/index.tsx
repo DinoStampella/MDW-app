f2ad4375b0486eae3237527fbd87bcc8339ef096
Dino <dino.stampella96@gmail.com>
2024-11-12 20:29:34 -0300

    Setup joi validations

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,6 +2,8 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../config/firebase";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { joiResolver } from "@hookform/resolvers/joi";
+import { signUpSchema } from "./validations";
 
 type FormValues = {
     email: string;
@@ -12,7 +14,9 @@ type FormValues = {
 const SignUp = () => {
     const navigate = useNavigate();
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<FormValues>();
+    const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({
+        resolver: joiResolver(signUpSchema)
+    });
 
     const handleSignUp = handleSubmit(async (data) => {
         try {
@@ -27,45 +31,17 @@ const SignUp = () => {
         <form onSubmit={handleSignUp}>
             <div>
                 <label>Email</label>
-                <input {...register("email", {
-                    required: {
-                        value: true,
-                        message: 'Email is required'
-                    },
-                    pattern: {
-                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                        message: 'Invalid email address'
-                    }
-                })}/>
+                <input {...register("email")}/>
                 {errors?.email && <p className="text-red-600 font-bold">{errors.email.message}</p>}
             </div>
             <div>
                 <label>Password</label>
-                <input {...register("password", {
-                    required: {
-                        value: true,
-                        message: 'Password is required'
-                    },
-                    minLength: {
-                        value: 6,
-                        message: 'Password must have at least 6 characters'
-                    },
-                })}/>
+                <input {...register("password")}/>
                 {errors?.password && <p className="text-red-600 font-bold">{errors.password.message}</p>}
             </div>
             <div>
                 <label>Repeat Password</label>
-                <input {...register("repeatPassword", {
-                    required: {
-                        value: true,
-                        message: 'Repeat Password is required'
-                    },
-                    minLength: {
-                        value: 6,
-                        message: 'Password must have at least 6 characters'
-                    },
-                    validate: (value) => value === watch("password")  || 'The passwords do not match'
-                })}/>
+                <input {...register("repeatPassword")}/>
                 {errors?.repeatPassword && <p className="text-red-600 font-bold">{errors.repeatPassword.message}</p>}
             </div>
             <button type="submit">
